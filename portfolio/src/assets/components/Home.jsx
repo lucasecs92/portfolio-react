d973bf305d5ff883f4c851d623e48b2975ffd280
Lucas Eduardo <102336205+lucasecs92@users.noreply.github.com>
2024-01-09 15:43:00 -0300

    update
    lapidando

--- portfolio/src/assets/components/Home.jsx
+++ portfolio/src/assets/components/Home.jsx
@@ -1,42 +1,43 @@
 import { useContext } from 'react';
 import styles from '../css/Home.module.css';
+
 import { BiLogoLinkedin } from "react-icons/bi";
 import { FiGithub } from "react-icons/fi";
 import ThemeContext from '../../contexts/ThemeContext'; 
 
 const Home = () => {
-   const themeContext = useContext(ThemeContext); 
+    const themeContext = useContext(ThemeContext); 
 
-   return (
-       <>
-           <section className={`${styles.containerHome} ${themeContext.theme === 'dark' ? styles.darkMode : ''}`}>
-               <section className={styles.description}>
-                  <article className={styles.descriptionText}>
-                      <p className={styles.greeting}>{"Olá!"}</p>
-                      <h3>Me chamo</h3>
-                      <h1>LUCAS EDUARDO</h1>
-                      <p className={styles.smallDescription}>
-                          Eu sou um <span>Desenvolvedor Front-end Junior</span> interessado em criar aplicações e experiências web interativas.
-                      </p>
-                      <section className={styles.socialMedia}>
-                          <ul className={styles.socialList}>
-                              <li>
-                                 <a href="https://www.linkedin.com/in/lucas-eduardo-cavalcante/" target="_blank" rel="noopener noreferrer"><BiLogoLinkedin/></a>
-                              </li>
+    return (
+        <>
+            <section className={`${styles.containerHome} ${themeContext.theme === 'dark' ? styles.darkMode : ''}`}>
+                <section className={styles.description}>
+                    <article className={styles.descriptionText}>
+                        <p className={styles.greeting}>{"</> Olá Mundo!"}</p>
+                        <h3>Me chamo</h3>
+                        <h1>LUCAS EDUARDO</h1>
+                        <p className={styles.smallDescription}>
+                            Eu sou um <span>Desenvolvedor Front-end Junior</span> interessado em criar aplicações e experiências web interativas.
+                        </p>
+                        <section className={styles.socialMedia}>
+                            <ul className={styles.socialList}>
+                                <li>
+                                    <a href="https://www.linkedin.com/in/lucas-eduardo-cavalcante/" target="_blank" rel="noopener noreferrer"><BiLogoLinkedin/></a>
+                                </li>
 
-                              <li>
-                                 <a href="https://github.com/lucasecs92" target="_blank" rel="noopener noreferrer"><FiGithub/></a>
-                              </li>
-                          </ul>
-                      </section>
-                  </article>
-                  <figure className={styles.mainPhoto}>
-                      <img src="https://github.com/lucasecs92.png" alt="Foto do meu perfil Github" />
-                  </figure>
-               </section>
-           </section> 
-       </>
-   )
+                                <li>
+                                    <a href="https://github.com/lucasecs92" target="_blank" rel="noopener noreferrer"><FiGithub/></a>
+                                </li>
+                            </ul>
+                        </section>
+                    </article>
+                    <figure className={styles.mainPhoto}>
+                        <img src="https://github.com/lucasecs92.png" alt="Foto do meu perfil Github" />
+                    </figure>
+                </section>
+            </section> 
+        </>
+    )
 }
 
 export default Home;
--- portfolio/src/assets/components/Projects.jsx
+++ portfolio/src/assets/components/Projects.jsx
@@ -8,99 +8,102 @@ import projeto3 from '../images/todolist.png';
 
 import { useInView } from 'react-intersection-observer';
 import { motion } from 'framer-motion';
+import { useContext } from 'react';
 
 const Projects = () => {
 
-   const [ref1, inView1] = useInView();
-   const [ref2, inView2] = useInView();
-   const [ref3, inView3] = useInView();
+    const themeContext = useContext(ThemeContext);
 
-   const [refImg1, inViewImg1] = useInView();
-   const [refImg2, inViewImg2] = useInView();
-   const [refImg3, inViewImg3] = useInView();
+    const [ref1, inView1] = useInView();
+    const [ref2, inView2] = useInView();
+    const [ref3, inView3] = useInView();
 
-   const variants = {
-       hidden: { opacity: 0, x: -100 }, 
-       show: {
-           opacity: 1,
-           x: 0, 
-           transition: {
-               duration: 2,
-               ease: "easeOut",
-           },
-       },
-   };
+    const [refImg1, inViewImg1] = useInView();
+    const [refImg2, inViewImg2] = useInView();
+    const [refImg3, inViewImg3] = useInView();
 
-   const imgVariants = {
-       hidden: { opacity: 0, x: 100 }, 
-       show: {
-           opacity: 1,
-           x: 0, 
-           transition: {
-               duration: 2,
-               ease: "easeOut",
-           },
-       },
-   };
-    
-   return (
-       <>
-           <section className={`${styles.containerProjects} ${ThemeContext.theme === 'dark' ? styles.darkMode : ''}`}>
+    const variants = {
+        hidden: { opacity: 0, x: -100 }, 
+        show: {
+            opacity: 1,
+            x: 0, 
+            transition: {
+                duration: 2,
+                ease: "easeOut",
+            },
+        },
+    };
 
-                  <section className={styles.title}>
-                        <h3>Projects</h3>
-                  </section>
+    const imgVariants = {
+        hidden: { opacity: 0, x: 100 }, 
+        show: {
+            opacity: 1,
+            x: 0, 
+            transition: {
+                duration: 2,
+                ease: "easeOut",
+            },
+        },
+    };
+        
+    return (
+        <>
+            <section className={`${styles.containerProjects} ${themeContext.theme === 'dark' ? styles.darkMode : ''}`}>
 
-                  <section className={styles.projects}>
-                        <section className={styles.projectsWrap}>
-                            <motion.aside ref={ref1} animate={inView1 ? variants.show : variants.hidden} variants={variants} className={styles.project1}>
-                                <h4 className={styles.projectName}>Book Store</h4>
-                                <p className={styles.projectDescription}>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis officia fugiat est consectetur aliquam eligendi qui provident esse, laudantium repellendus dicta commodi ut nihil quasi, repellat quaerat, temporibus sed corrupti!
-                                </p>
-                                <a className={styles.projectBtn} href="https://book-store-rho-ten.vercel.app/" target='_blank' rel='noopener noreferrer'>
-                                    Visit Website
-                                </a>
-                            </motion.aside>
-                            <motion.figure ref={refImg1} animate={inViewImg1 ? "show" : "hidden"} variants={imgVariants} className={styles.projectImg}>
-                                <img src={projeto1} alt="imagem de capa do bookstore" />
-                            </motion.figure>
-                        </section>
+                    <section className={styles.title}>
+                            <h3>Projetos</h3>
+                    </section>
 
-                        <section className={styles.projectsWrapReverse}>
-                            <motion.aside ref={ref2} animate={inView2 ? imgVariants.show : imgVariants.hidden} variants={imgVariants} className={styles.project2}>
-                                <h4 className={styles.projectName}>Weather</h4>
-                                <p className={styles.projectDescription}>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis officia fugiat est consectetur aliquam eligendi qui provident esse, laudantium repellendus dicta commodi ut nihil quasi, repellat quaerat, temporibus sed corrupti!
-                                </p>
-                                <a className={styles.projectBtnReverse} href="https://weather-navy-xi.vercel.app/" target='_blank' rel='noopener noreferrer'>
-                                    Visit Website
-                                </a>
-                            </motion.aside>
+                    <section className={styles.projects}>
+                            <section className={styles.projectsWrap}>
+                                <motion.aside ref={ref1} animate={inView1 ? variants.show : variants.hidden} variants={variants} className={styles.project1}>
+                                    <h4 className={styles.projectName}>Book Store</h4>
+                                    <p className={styles.projectDescription}>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis officia fugiat est consectetur aliquam eligendi qui provident esse, laudantium repellendus dicta commodi ut nihil quasi, repellat quaerat, temporibus sed corrupti!
+                                    </p>
+                                    <a className={styles.projectBtn} href="https://book-store-rho-ten.vercel.app/" target='_blank' rel='noopener noreferrer'>
+                                        Visualizar Website
+                                    </a>
+                                </motion.aside>
+                                <motion.figure ref={refImg1} animate={inViewImg1 ? "show" : "hidden"} variants={imgVariants} className={styles.projectImg}>
+                                    <img src={projeto1} alt="imagem de capa do bookstore" />
+                                </motion.figure>
+                            </section>
 
-                            <motion.figure ref={refImg2} animate={inViewImg2 ? "show" : "hidden"} variants={variants} className={styles.projectImgReverse}>
-                                <img src={projeto2} alt="imagem de capa do projeto" />
-                            </motion.figure>
-                        </section>
+                            <section className={styles.projectsWrapReverse}>
+                                <motion.aside ref={ref2} animate={inView2 ? imgVariants.show : imgVariants.hidden} variants={imgVariants} className={styles.project2}>
+                                    <h4 className={styles.projectName}>Weather</h4>
+                                    <p className={styles.projectDescription}>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis officia fugiat est consectetur aliquam eligendi qui provident esse, laudantium repellendus dicta commodi ut nihil quasi, repellat quaerat, temporibus sed corrupti!
+                                    </p>
+                                    <a className={styles.projectBtnReverse} href="https://weather-navy-xi.vercel.app/" target='_blank' rel='noopener noreferrer'>
+                                        Visualizar Website
+                                    </a>
+                                </motion.aside>
 
-                        <section className={styles.projectsWrap}>
-                            <motion.aside ref={ref3} animate={inView3 ? variants.show : variants.hidden} variants={variants} className={styles.project3}>
-                                <h4 className={styles.projectName}>To Do List</h4>
-                                <p className={styles.projectDescription}>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis officia fugiat est consectetur aliquam eligendi qui provident esse, laudantium repellendus dicta commodi ut nihil quasi, repellat quaerat, temporibus sed corrupti!
-                                </p>
-                                <a className={styles.projectBtn} href="https://todolist-bay-five.vercel.app/" target='_blank' rel='noopener noreferrer'>
-                                    Visit Website
-                                </a>
-                            </motion.aside>
-                            <motion.figure ref={refImg3} animate={inViewImg3 ? "show" : "hidden"} variants={imgVariants} className={styles.projectImg}>
-                                <img src={projeto3} alt="imagem de capa do projeto" />
-                            </motion.figure>
-                        </section> 
-                  </section>
-           </section>
-       </>
-   )
+                                <motion.figure ref={refImg2} animate={inViewImg2 ? "show" : "hidden"} variants={variants} className={styles.projectImgReverse}>
+                                    <img src={projeto2} alt="imagem de capa do projeto" />
+                                </motion.figure>
+                            </section>
+
+                            <section className={styles.projectsWrap}>
+                                <motion.aside ref={ref3} animate={inView3 ? variants.show : variants.hidden} variants={variants} className={styles.project3}>
+                                    <h4 className={styles.projectName}>To Do List</h4>
+                                    <p className={styles.projectDescription}>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis officia fugiat est consectetur aliquam eligendi qui provident esse, laudantium repellendus dicta commodi ut nihil quasi, repellat quaerat, temporibus sed corrupti!
+                                    </p>
+                                    <a className={styles.projectBtn} href="https://todolist-bay-five.vercel.app/" target='_blank' rel='noopener noreferrer'>
+                                        Visualizar Website
+                                    </a>
+                                </motion.aside>
+                                <motion.figure ref={refImg3} animate={inViewImg3 ? "show" : "hidden"} variants={imgVariants} className={styles.projectImg}>
+                                    <img src={projeto3} alt="imagem de capa do projeto" />
+                                </motion.figure>
+                            </section> 
+                    </section>
+            </section>
+        </>
+    )
 }
 
 export default Projects;
